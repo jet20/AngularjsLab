31a208ddcb2e50200c255adda7a4a2f1f386febf
liuzhaohui <liuzhaohui@ithinker.com.cn>
2018-05-29 11:11:36 +0800

    npm i bootstrap@3

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.use('/lib', express.static('bower_components'));
+app.use('/lib', express.static('node_modules'));
+// app.use('/lib', express.static('bower_components'));
 
 app.get('/', function (req, res) {
   res.redirect('/index.html');
