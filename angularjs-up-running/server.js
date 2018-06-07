60edc175ec2fac479dc88c68ad05a4e8bfa06beb
liuzhaohui <liuzhaohui@ithinker.com.cn>
2018-06-07 18:01:36 +0800

    up & running

---
+++ angularjs-up-running/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('angularjs-up-running'));
+app.use('/lib', express.static('lib'));
+
+app.get('/', function (req, res) {
+  res.redirect('/index.html');
+});
+
+app.listen(3000, function () {
+  console.log('url: http://localhost:3000');
+});
---
+++ angularjs-up-running/tests/chapter03/controlerSpec.js
@@ -0,0 +1,11 @@
+describe('Controller: ListCtrl', function() {
+	beforeEach(module('notesApp'));
+
+	var ctrl;
+
+	beforeEach(inject(function($controller){
+		ctrl = $controller('ListCtrl');
+	}));
+
+	
+});
